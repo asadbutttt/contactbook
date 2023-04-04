1b4dc212503874458ca89c6cb47808618e3ef1bd
Asadullah <asadullah.butt911@live.com>
2023-04-04 15:34:19 +0500

    Changes allowed hosts

--- contactbook/settings.py
+++ contactbook/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-e^t&+gzf7xzxjw94xu0n37j-d2a*x&5$t$q3ii$=-q8nckv8pz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
